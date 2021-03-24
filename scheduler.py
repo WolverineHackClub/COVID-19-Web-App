f737ec3d4525ba6a368bdf2bf240af713e222928
Cooper Werner <cooperwerner824@gmail.com>
2021-03-24 10:14:03 -0400

    Scheduler Bug Fixes and Exception Handling
    + Added Exception Handling to critical parts of scheduler
    + Fixed Flask Server Bug

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,18 @@
 import subprocess
 import datetime
 from time import strftime, sleep
+import logging
+
+# creating error logs
+logging.basicConfig(filename='static/error_log.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+logger=logging.getLogger(__name__)
 
 #run the scraper code first
 subprocess.run(["python3", "scraper.py"]
+
+#Flask Setup
+subprocess.run(["export", "FLASK_APP=server.py"])
 
 #set the restart time to be  23:59
 restartTime = datetime.time(23,59,00).strftime('%H:%M:%S')
@@ -18,7 +27,11 @@ exitCondition = False
 while(!exitCondition):
 
     try:
-        server = subprocess.popen(["python3", "server.py"]) #create server subprocess, this is what im most concerned about
+        try:
+            server = subprocess.popen(["flask", "run"]) #create server subprocess, this is what im most concerned about
+        except Exception as err:
+            logger.error(err)
+
         while (!restartCondition): #wait until time is correct
             localTime = datetime.datetime.now().time().strftime('%H:%M:%S') # get string local time
             if localTime == restartTime: # if time is correct then we stop the server
@@ -28,8 +41,11 @@ while(!exitCondition):
                 restartCondition = True
             time.sleep(1)
 
-        server.terminate() #terminate the server process
-        subprocess.run(["python3", "scraper.py"]) # run the scraper program
+        try:
+            server.terminate() #terminate the server process
+            subprocess.run(["python3", "scraper.py"]) # run the scraper program
+        except Exception as err:
+            logger.error(err)
 
     except KeyboardInterrupt: # check for KeyboardInterrupt (CTRL+C)
         print("Stopping Scheduler")
